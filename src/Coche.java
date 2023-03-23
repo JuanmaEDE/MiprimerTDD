17d143ebfe841825ab862576caa090e2b4f0f88f
JuanmaEDE <juanmaclaseinformatica@gmail.com>
2023-03-23 20:03:46 +0100

    Realizado Refactorizado a todos los métodos con mi nombre y apellidos

--- src/Coche.java
+++ src/Coche.java
@@ -1,11 +1,11 @@
 public class Coche {
     public int velocidad;
 
-    public void acelerar(int aceleracion) {
+    public void acelerar_JuanManuel_Belaunde_Martinez(int aceleracion) {
         velocidad += aceleracion;
     }
 
-    public void decelerar(int deceleracion) {
+    public void decelerar_JuanManuel_Belaunde_Martinez(int deceleracion) {
         velocidad -= deceleracion;
         if (velocidad <0)velocidad =0;
     }
